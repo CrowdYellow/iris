eff054bdd712115964f52b8d9eea2bb09681e22c
CrowdYellow <echo61546@gmail.com>
2019-06-06 16:21:59 +0800

    添加数据库

---
+++ config/db.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"github.com/go-xorm/core"
+	"github.com/go-xorm/xorm"
+	"log"
+)
+
+func DBConnect() *xorm.Engine {
+	engine, err := xorm.NewEngine(DB_CONNECTION, DB_USERNAME+":"+DB_PASSWORD+"@/"+DB_DATABASE+"?charset=utf8")
+	if err != nil {
+		log.Print("open mysql failed,err = ", err)
+	}
+
+	// 名称映射 SnakeMapper支持struct为驼峰式命名，表结构为下划线命名之间的转换
+	engine.SetTableMapper(core.SnakeMapper{})
+
+	return engine
+}
---
+++ config/env.go
@@ -0,0 +1,10 @@
+package config
+
+const (
+	DB_CONNECTION = "mysql"
+	DB_HOST       = "127.0.0.1"
+	DB_PORT       = "3306"
+	DB_DATABASE   = "golang"
+	DB_USERNAME   = "golang"
+	DB_PASSWORD   = "golang"
+)
